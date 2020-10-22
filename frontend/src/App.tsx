f1d65477daacec231bb4093f36c32d0ab31a2695
Joel Binnquist <joel.binnquist@gmail.com>
2020-10-22 23:45:47 +0200

    Button for ping

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,17 @@
 import React from 'react';
-import {useState, useEffect} from 'react';
+import {useState} from 'react';
 import './App.css';
 
 function App() {
+  async function doPing() {
+    const response = await fetch('/api/v1/ping')
+    const text = await response.text();
+    console.log("text:", text);
+    setPingResult(text)
+  };
+
   const [pingResult, setPingResult] = useState<string|undefined>()
-  useEffect(() => {
-    async function doPing() {
-      const response = await fetch('/api/v1/ping')
-      const text = await response.text();
-      console.log("text:", text);
-      setPingResult(text)
-    };
-    doPing();
-  });
+
   return (
     <div className="app">
       <header>
@@ -21,13 +20,16 @@ function App() {
         </div>
       </header>
       <section>
-      <div className="container">
-        <p>Sending ping to /api/v1/ping</p>
-        <p>Got response: <span className="highlight"><strong>{pingResult}</strong></span></p>
-      </div>
+        <div className="container">
+          <p>
+            <button className="btn btn-primary" onClick={doPing}>Send ping to /api/v1/ping</button>
+          </p>
+          {pingResult && <p>Got response: <span className="highlight"><strong>{pingResult}</strong></span></p>}
+        </div>
       </section>
     </div>
   );
+
 }
 
 export default App;
